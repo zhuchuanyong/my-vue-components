d77f187506ae0ade840516001d63a178810a44b7
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-09-24 23:25:01 +0800

    first

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import "@style/index.scss";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Antd)
+  .mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <a-button type="primary" @click="addNum">
+      加1
+    </a-button>
+    <span class="mx-1">counter:{{ counter }}</span>
+    <a-button type="primary" @click="minusNum">
+      减1
+    </a-button>
+  </div>
+  <div class="flex">
+    <FormOne class="flex-1" msg="Welcome to Your Vue.js + TypeScript App" />
+    <FormOne class="flex-1" msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import FormOne from "@/components/FormOne.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    FormOne
+  },
+  setup() {
+    const counter = ref<number>(0);
+    const addNum = () => {
+      console.log("counter", counter);
+      counter.value++;
+    };
+    const minusNum = () => {
+      console.log("counter", counter);
+      counter.value--;
+    };
+    return { counter, addNum, minusNum };
+  }
+});
+</script>
